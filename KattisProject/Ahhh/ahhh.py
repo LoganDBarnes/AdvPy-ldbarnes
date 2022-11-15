cb5a17cbb74aab9db7ede80c357a9ce7df89ee54
Logan Barnes <ldbarnes@hotmail.com>
2022-11-15 02:22:50 -0700

    Ahhh initial solution

---
+++ KattisProject/Ahhh/ahhh.py
@@ -0,0 +1,17 @@
+# Logan Barnes
+# Ahhh
+
+# Initialize
+patient = input()
+doctor = input()
+
+patientAs = patient.count('a')
+doctorAs = doctor.count('a')
+
+# print(patientAs)
+# print(doctorAs)
+
+if patientAs >= doctorAs:
+    print("go")
+else:
+    print("no")
