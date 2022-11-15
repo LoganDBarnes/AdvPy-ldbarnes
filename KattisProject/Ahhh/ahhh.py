504b60270fb818cc6a8e899f358c5760d76e15e4
Logan Barnes <ldbarnes@hotmail.com>
2022-11-15 02:27:01 -0700

    Ahhh accepted

--- KattisProject/Ahhh/ahhh.py
+++ KattisProject/Ahhh/ahhh.py
@@ -1,17 +1,22 @@
 # Logan Barnes
 # Ahhh
 
-# Initialize
-patient = input()
-doctor = input()
+def compareAs():
+    # Initialize
+    patient = input()
+    doctor = input()
 
-patientAs = patient.count('a')
-doctorAs = doctor.count('a')
+    patientAs = patient.count('a')
+    doctorAs = doctor.count('a')
 
-# print(patientAs)
-# print(doctorAs)
+    # print(patientAs)
+    # print(doctorAs)
 
-if patientAs >= doctorAs:
-    print("go")
-else:
-    print("no")
+    if patientAs >= doctorAs:
+        print("go")
+    else:
+        print("no")
+
+    return 0
+
+compareAs()
