43ac908d96ba6e95b524c9d22354b2b69f6b198e
Logan Barnes <ldbarnes@hotmail.com>
2022-12-13 19:42:44 -0700

    Initial solution

---
+++ Final/RightOfWay/rightOfWay.py
@@ -0,0 +1,85 @@
+# Logan Barnes
+# Kattis
+# Right of Way
+
+# Program to determine right of way for self-driving car
+# Given direction car is approaching intersection, direction leaving intersection, and direction other vehicle is approaching intersection
+# Car must yield right of way if it is going straight and there is a vehicle approaching from the right
+# Car must yield right of way if it is turning left and there is a vehicle approaching from the right or straight ahead
+# Output Yes if car must yield right of way, No if car has right of way
+
+
+carEnter, carLeave, vehicleApproach = input().split()
+
+def goingStraight():
+    straight = False
+
+    if carEnter == 'North' and carLeave == 'South':
+        straight = True
+    elif carEnter == 'East' and carLeave == 'West':
+        straight = True
+    elif carEnter == 'South' and carLeave == 'North':
+        straight = True
+    elif carEnter == 'West' and carLeave == 'East':
+        straight = True
+    
+    #print("going straight " + str(straight))
+    return straight
+
+def turningLeft():
+    left = False
+
+    if carEnter == 'North' and carLeave == 'East':
+        left = True
+    elif carEnter == 'East' and carLeave == 'South':
+        left = True
+    elif carEnter == 'South' and carLeave == 'West':
+        left = True
+    elif carEnter == 'West' and carLeave == 'North':
+        left = True
+    
+    #print("turning left " + str(left))
+    return left
+
+def vehicleAhead():
+    ahead = False
+
+    if carEnter == 'North' and vehicleApproach == 'South':
+        ahead = True
+    elif carEnter == 'East' and vehicleApproach == 'West':
+        ahead = True
+    elif carEnter == 'South' and vehicleApproach == 'North':
+        ahead = True
+    elif carEnter == 'West' and vehicleApproach == 'East':
+        ahead = True
+    
+    #print("vehicle ahead " + str(ahead))
+    return ahead
+
+def vehicleRight():
+    right = False
+
+    if carEnter == 'North' and vehicleApproach == 'West':
+        right = True
+    elif carEnter == 'East' and vehicleApproach == 'North':
+        right = True
+    elif carEnter == 'South' and vehicleApproach == 'East':
+        right = True
+    elif carEnter == 'West' and vehicleApproach == 'South':
+        right = True
+
+    #print("vehicle right " + str(right))
+    return right
+
+def main():
+    
+        if goingStraight() and vehicleRight():
+            print('Yes')
+        elif turningLeft() and (vehicleRight() or vehicleAhead()):
+            print('Yes')
+        else:
+            print('No')
+    
+        return 0
+
+main()
